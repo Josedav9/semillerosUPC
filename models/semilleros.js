dda8277a378215d11d9131c1f539729d6f98a5f3
Jose alvarez <josedav_96@hotmail.com>
2018-03-21 19:42:17 -0500

    Primera fase

---
+++ models/semilleros.js
@@ -0,0 +1,29 @@
+let semillero = function(sequelize, Sequelize) {
+  let modelo = sequelize.define(
+    "Semilleros",{
+      "idSemillero":{
+        primaryKey:true,
+        autoIncrement:true,
+        type:Sequelize.INTEGER
+      },
+      "nombreSemillero":{
+        notEmpty:true,
+        type:Sequelize.STRING
+      },
+      "descripcionSemillero":{
+        notEmpty:true,
+        type:Sequelize.STRING
+      },
+      "liderSemillero":{
+        notEmpty:true,
+        type:Sequelize.STRING
+      }
+    },
+    {
+      timestamps:false,
+      freezeTableName:true
+    }
+  );
+  return modelo;
+};
+module.exports = semillero;
